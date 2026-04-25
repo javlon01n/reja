7568c306b9bc4a838a1343cfd130bd37e73cb3f9
javlon01n <joxa030@gmail.com>
2026-04-25 19:00:19 +0900

    feat: mitask  add

--- train.js
+++ train.js
@@ -16,41 +16,41 @@ console.log(number("engineer", "e"));
 
 
 
-console.log("Jack Ma maslahatlari");
-const list = [
-    "yaxsh talaba bo'ling", //0-20
-    "togri boshliq tanlang va koproq xato qiling", //20-30
-    "uzingizga ishlashingizni boshlang", //30-40
-    "siz kuchli bolgan narsalarni qiling", //40-50
-    "yoshlarga invistitsiya qiling", //50-60
-    "endi dam oling, foydasi yoq endi", //60
-];
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//     "yaxsh talaba bo'ling", //0-20
+//     "togri boshliq tanlang va koproq xato qiling", //20-30
+//     "uzingizga ishlashingizni boshlang", //30-40
+//     "siz kuchli bolgan narsalarni qiling", //40-50
+//     "yoshlarga invistitsiya qiling", //50-60
+//     "endi dam oling, foydasi yoq endi", //60
+// ];
 
-//COLLBACK fungtion
-function maslahatBering(a, callback) {
-    if(typeof a !== 'number') callback("insert a nomber", null);
-    else if(a <= 20) callback(null,list[0]);
-    else if(a >20 && a <=30) callback(null, list[1])
-    else if(a >30 && a <=40) callback(null, list[2])
-    else if(a >40 && a <=50) callback(null, list[3])
-    else if(a >50 && a <=60) callback(null, list[4])
-    else {
-     setInterval(function () {
-        callback(null, list[5]);
-      }, 1000)
+// //COLLBACK fungtion
+// function maslahatBering(a, callback) {
+//     if(typeof a !== 'number') callback("insert a nomber", null);
+//     else if(a <= 20) callback(null,list[0]);
+//     else if(a >20 && a <=30) callback(null, list[1])
+//     else if(a >30 && a <=40) callback(null, list[2])
+//     else if(a >40 && a <=50) callback(null, list[3])
+//     else if(a >50 && a <=60) callback(null, list[4])
+//     else {
+//      setInterval(function () {
+//         callback(null, list[5]);
+//       }, 1000)
     
-    }
-}
+//     }
+// }
 
 
-console.log("passed here 0");
-maslahatBering(65, (err, data) => {
-    if(err) console.log('ERROR', err);
-    else{
-    console.log('javob:', data);
-    }
-});
-console.log("passed here 1");
+// console.log("passed here 0");
+// maslahatBering(65, (err, data) => {
+//     if(err) console.log('ERROR', err);
+//     else{
+//     console.log('javob:', data);
+//     }
+// });
+// console.log("passed here 1");
 
 
 //ASYNC fungtion
