6865b6862f8432fba3738d77e2b9ed56a3277c43
javlon01n <joxa030@gmail.com>
2026-04-27 18:34:58 +0900

    fix: refactoring

---
+++ app.js
@@ -0,0 +1,54 @@
+console.log("Web serverni boshlash");
+const express = require("express"); //express nima? bu: Node.s uchun server yaratish framework’i, Node.js → oddiy dvigatel Express → tayyor mashina 
+const app = express(); 
+const http = require("http");
+const fs = require("fs");
+
+//MongoDB connect
+
+
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    user = [];
+  } else {
+    try {
+      user = JSON.parse(data || "[]");
+    } catch {
+      user = [];
+    }
+  }
+});
+
+
+// 1: kirish kodlari
+  //expressga kirib kelayotgan malumotlarga bo'liq bo'lgan kodlar
+app.use(express.static("public")); //xar qanday braozerdan kirib kelayotgan malumotlardan faqat public folderni ko'raoladi
+app.use(express.json()); //kirib kelayotgan json formatdagi datani object formatga alamshtirib beradi
+app.use(express.urlencoded({extended: true})); //HTML formdan post qilingan narsalarni qabul qilish uchun kk
+
+// 2: Session code
+// 3: views code     views → frontend sahifalar
+app.set("views", "views"); //HTML frontentni yasaymiz viewni ichida
+app.set("view engine", "ejs"); //BSSR  //.ejs → HTML + JS    //res.render() → sahifani chiqaradi
+
+// 4: Routing code
+app.post("/create-item", (req, res) => {
+    // console.log(req.body);
+    // res.json({test: "success"});
+});
+
+app.get('/author', (req, res) => {
+  res.render("author", {user: user } );
+});
+
+app.get("/", function (req , res) {
+ res.render("reja");
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function () {
+console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+
+);
+});
